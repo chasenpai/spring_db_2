2c19f7225a0e23f32d2024381093c5054e59ec72
junwvwv <wvwv0612@gmail.com>
2023-09-02 14:16:42 +0900

    테스트 시 트랜잭션 처리

--- itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -5,19 +5,50 @@ import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
 import hello.itemservice.repository.memory.MemoryItemRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional //테스트에서 사용 시 트랜잭션을 자동으로 롤백시켜준다
 class ItemRepositoryTest {
 
+    /**
+     * 테스트에서 매우 중요한 원칙
+     * - 테스트는 다른 테스트와 격리해야 한다
+     * - 테스트는 반복해서 실행할 수 있어야 한다
+     *
+     * - 처음 테스트 실행 시 저장한 데이터가 계속 남아있으면 다음 테스트에 영향을 준다
+     * - 이 문제를 해결하려면 각각의 테스트가 끝날 때 마다 데이터를 삭제해줘야 한다
+     * - 결과적으로 테스트 데이터가 오염된 것이다
+     * - 이를 해결하기 위해 트랜잭션과 롤백 전략을 사용하면 된다
+     *
+     * - 리포지토리나 서비스에 있는 @Transactional 도 테스트에서 시작한 트랜잭션에 참여한다
+     * - 테스트에서 트랜잭션을 시작하면 테스트 실행이 종료될 때 까지 모든 코드가 같은 트랜잭션 범위에 들어간다
+     */
     @Autowired
     ItemRepository itemRepository;
+
+//    @Autowired
+//    PlatformTransactionManager transactionManager;
+//    TransactionStatus status;
+
+//    @BeforeEach
+//    void beforeEach() {
+        //트랜잭션 시작
+//        status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+//    }
 
     @AfterEach
     void afterEach() {
@@ -25,9 +56,15 @@ class ItemRepositoryTest {
         if (itemRepository instanceof MemoryItemRepository) {
             ((MemoryItemRepository) itemRepository).clearStore();
         }
+
+        //트랜잭션 롤백
+//        transactionManager.rollback(status);
     }
 
     @Test
+//    @Transactional //데이터를 DB 에서 직접 눈으로 확인하고 싶으면..
+//    @Commit //강제로 커밋하기
+//    @Rollback(value = false) //롤백 미사용
     void save() {
         //given
         Item item = new Item("itemA", 10000, 10);
