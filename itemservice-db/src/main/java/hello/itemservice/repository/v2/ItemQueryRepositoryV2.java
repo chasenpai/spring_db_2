651afd38f4104b8144588c2a5004a1024ab956c8
junwvwv <wvwv0612@gmail.com>
2023-09-07 19:49:54 +0900

    실용적인 구조

---
+++ itemservice-db/src/main/java/hello/itemservice/repository/v2/ItemQueryRepositoryV2.java
@@ -0,0 +1,47 @@
+package hello.itemservice.repository.v2;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemSearchCond;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static hello.itemservice.domain.QItem.item;
+
+@Repository
+public class ItemQueryRepositoryV2 {
+
+    private final JPAQueryFactory queryFactory;
+
+    public ItemQueryRepositoryV2(EntityManager entityManager){
+        this.queryFactory = new JPAQueryFactory(entityManager);
+    }
+
+    public List<Item> findAll(ItemSearchCond cond) {
+        return queryFactory.select(item)
+                .from(item)
+                .where(
+                        maxPrice(cond.getMaxPrice()),
+                        likeItemName(cond.getItemName()))
+                .fetch();
+    }
+
+    private BooleanExpression likeItemName(String itemName) {
+        if (StringUtils.hasText(itemName)) {
+            return item.itemName.like("%" + itemName + "%");
+        }
+        return null;
+    }
+
+    private BooleanExpression maxPrice(Integer maxPrice) {
+        if (maxPrice != null) {
+            return item.price.loe(maxPrice);
+        }
+        return null;
+    }
+
+}
