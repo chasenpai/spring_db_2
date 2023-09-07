f60d04d53aaa5af1c7d7f85d0769c87eedcf1a7c
junwvwv <wvwv0612@gmail.com>
2023-09-07 19:38:22 +0900

    트레이드 오프 설명

--- itemservice-db/src/main/java/hello/itemservice/repository/jpa/JpaItemRepositoryV2.java
+++ itemservice-db/src/main/java/hello/itemservice/repository/jpa/JpaItemRepositoryV2.java
@@ -23,6 +23,25 @@ public class JpaItemRepositoryV2 implements ItemRepository {
      * - ItemService 가 SpringDataJpaItemRepository 를 직접 사용하도록 코드를 고치면 된다
      * - 하지만 기존 코드의 변경 없이 DI 를 통해 구현 기술을 변경하는 것이 좋다
      * - JpaItemRepositoryV2 가 ItemRepository 와 SpringDataJpaItemRepository 사이를 맞추기 위한 어댑터 처럼 사용된다
+     *
+     * 하지만?
+     * - 구조를 맞추기 위해서 중간에 어댑터가 들어가게 되니 전체 구조가 복잡해지고 사용하는 클래스가 많아졌다
+     * - 유지보수 관점에서 ItemService 를 변경하지 않고 ItemRepository 의 구현체를 변경할 수 있는 장점이 있지만
+     * - 이는 DI, OCP 원칙을 지킬 수 있는 좋은 점이지만, 반대로 구조가 복잡해지면서 어댑터 코드와 실제 코드가지
+     * - 함께 유지보수 해야 하는 어려움도 발생한다
+     *
+     * 다른 선택
+     * - 완전히히다른 선택으로 ItemService 코드 일부를 고쳐서 직접 SpringDataJpaItemRepository 를 사용하는 방법이다
+     * - DI, OCP 원칙을 포기하는 대신에 복잡한 어댑터를 제거하고 구조를 단순하게 가져갈 수 있는 장점이 있다
+     *
+     * 트레이드 오프
+     * - 위와 같은 상황을 트레이드 오프라고 한다
+     * 1. DI, OCP 를 지키기 위해 어댑터를 도입하고 더 많은 코드를 유지한다
+     * 2. 어댑터를 제거하고 구조를 단순하게 가져가지만, DI, OCP 를 포기한다
+     * - 이는 구조의 안전성 VS 단순한 구조와 개발의 편리성의 차이
+     * - 상황에 따라 더 나은 것을 선택해야 한다
+     * - 어설픈 추상화는 오히려 독이되고, 추상화도 비용이 들기 때문에 이 추상화가 비용을 넘어설 만큼의
+     * - 효과가 있을 때 추상화를 도입하는 것이 실용적이다
      */
     private final SpringDataJpaItemRepository repository;
 
